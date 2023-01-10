a0c34dc6cd05bc997817dd0f21217817e79486b2
balazs955 <83276057+balazs955@users.noreply.github.com>
2023-01-10 11:36:59 +0100

    Update forint.js

--- forint.js
+++ forint.js
@@ -1,10 +1,10 @@
 function forintszamol(){
     var ft = parseInt(document.getElementById("forint").value)
 
-    document.write("Euróban: " + ft * 404)
+    document.write("Euróban: " + ft / 404)
     document.write("<br>")
-    document.write("Dollárban: " + ft * 376.48)
+    document.write("Dollárban: " + ft / 376.48)
     document.write("<br>")
-    document.write("Japán jenben:" + ft * 2.88)
+    document.write("Japán jenben:" + ft / 2.88)
   
 }
