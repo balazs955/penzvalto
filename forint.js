1ecfe6c3e2dc1ea986047fc1654852bddb31cad1
Berc57 <83001491+Berc57@users.noreply.github.com>
2023-01-10 10:27:08 +0100

    Create forint.js

---
+++ forint.js
@@ -0,0 +1,10 @@
+function forint(){
+    var ft = parseInt(document.getElementById("forint").value)
+
+    document.write("Euróban: " + ft * 404)
+    document.write("<br>")
+    document.write("Dollárban: " + ft * 376.48)
+    document.write("<br>")
+    document.write("Japán jenben:" + ft * 2.88)
+  
+}
